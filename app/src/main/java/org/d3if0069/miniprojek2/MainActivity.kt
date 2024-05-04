b24fdaca60b7fa9ce99a1a93c8f74bd91d91676c
Raendhana Ilmi Vadhiasih <raendhanaiv@student.telkomuniversity.ac.id>
2024-05-04 12:21:31 +0700

    initial commit

---
+++ app/src/main/java/org/d3if0069/miniprojek2/MainActivity.kt
@@ -0,0 +1,46 @@
+package org.d3if0069.miniprojek2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0069.miniprojek2.ui.theme.Miniprojek2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Miniprojek2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Miniprojek2Theme {
+        Greeting("Android")
+    }
+}
