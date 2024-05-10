e8ee106991beca08137254581a88a44919237483
Raendhana Ilmi Vadhiasih <raendhanaiv@student.telkomuniversity.ac.id>
2024-05-10 22:45:15 +0700

    MiniProjek2

--- app/src/main/java/org/d3if0069/miniprojek2/ui/screen/DisplayAlertDialog.kt
+++ app/src/main/java/org/d3if0069/miniprojek2/ui/screen/DisplayAlertDialog.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if0069.miniprojek2.R
